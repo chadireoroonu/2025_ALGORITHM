13438dee8c8cb0ed34c01371c63085c871764715
chadireoroonu <chadireoroonu@gmail.com>
2025-07-23 23:36:05 +0900

    2507233 C++ refactoring

--- 07/C23_p_176963.cpp
+++ 07/C23_p_176963.cpp
@@ -11,17 +11,43 @@ vector<int> solution(vector<string> name, vector<int> yearning, vector<vector<st
     vector<int> answer(photo.size(), 0);
     map<string, int> score;
     
-    for (int i = 0; i < name.size(); i++) { // 그리운 사람의 그리움 점수 묶기
+    for (int i = 0; i < name.size(); i++) {
         score[name[i]] = yearning[i];
     }
     
-    for (int i = 0; i < photo.size(); i++) { // 각 사진
-        for (int j = 0; j < photo[i].size(); j++) { // 사진 한 장 속 각 인물
-            if (score.count(photo[i][j])) { // 그리운 사람인지 확인
-                answer[i] += score.at(photo[i][j]); // 그리움 점수 더하기
+    for (int i = 0; i < photo.size(); i++) {
+        for (const string& person : photo[i]) {
+            auto n = score.find(person);
+            if (n != score.end()) {
+                answer[i] += n -> second;
             }
         }
     }
     
     return answer;
 }
+
+// #include <string>
+// #include <vector>
+// #include <map>
+
+// using namespace std;
+
+// vector<int> solution(vector<string> name, vector<int> yearning, vector<vector<string>> photo) {
+//     vector<int> answer(photo.size(), 0);
+//     map<string, int> score;
+    
+//     for (int i = 0; i < name.size(); i++) { // 그리운 사람의 그리움 점수 묶기
+//         score[name[i]] = yearning[i];
+//     }
+    
+//     for (int i = 0; i < photo.size(); i++) { // 각 사진
+//         for (int j = 0; j < photo[i].size(); j++) { // 사진 한 장 속 각 인물
+//             if (score.count(photo[i][j])) { // 그리운 사람인지 확인
+//                 answer[i] += score.at(photo[i][j]); // 그리움 점수 더하기
+//             }
+//         }
+//     }
+    
+//     return answer;
+// }
