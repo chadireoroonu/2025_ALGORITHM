4848bc0828904c5b6a983deb5b21d42e93425261
chadireoroonu <chadireoroonu@gmail.com>
2025-07-23 22:28:36 +0900

    2507233 C++

---
+++ 07/C23_p_176963.cpp
@@ -0,0 +1,27 @@
+// 추억 점수
+// https://school.programmers.co.kr/learn/courses/30/lessons/176963
+
+#include <string>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+vector<int> solution(vector<string> name, vector<int> yearning, vector<vector<string>> photo) {
+    vector<int> answer(photo.size(), 0);
+    map<string, int> score;
+    
+    for (int i = 0; i < name.size(); i++) { // 그리운 사람의 그리움 점수 묶기
+        score[name[i]] = yearning[i];
+    }
+    
+    for (int i = 0; i < photo.size(); i++) { // 각 사진
+        for (int j = 0; j < photo[i].size(); j++) { // 사진 한 장 속 각 인물
+            if (score.count(photo[i][j])) { // 그리운 사람인지 확인
+                answer[i] += score.at(photo[i][j]); // 그리움 점수 더하기
+            }
+        }
+    }
+    
+    return answer;
+}
