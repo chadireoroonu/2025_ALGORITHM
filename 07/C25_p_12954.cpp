e829e3718b45662b73f72d79af132f7e61b26586
chadireoroonu <chadireoroonu@gmail.com>
2025-07-25 15:48:17 +0900

    2507255 C++

---
+++ 07/C25_p_12954.cpp
@@ -0,0 +1,17 @@
+// x만큼 간격이 있는 n개의 숫자
+// https://school.programmers.co.kr/learn/courses/30/lessons/12954
+
+
+#include <vector>
+
+using namespace std;
+
+vector<long long> solution(int x, int n) {
+    vector<long long> answer;
+    
+    for (int i = 1; i <= n; i++) {
+        answer.push_back((long long)x * i);
+    }
+    
+    return answer;
+}
