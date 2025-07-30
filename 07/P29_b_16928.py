b12753e896a3d165e9f12a1e6ff5f6c11646fc99
chadireoroonu <chadireoroonu@gmail.com>
2025-07-30 22:22:38 +0900

    2507293 refactoring

--- 07/P29_b_16928.py
+++ 07/P29_b_16928.py
@@ -5,6 +5,32 @@
 import sys
 from collections import deque
 
+# N, M = map(int, sys.stdin.readline().split())
+# shift = {}
+# count = [100] * 101
+
+# for _ in range(N + M):
+#     s, e = map(int, sys.stdin.readline().split())
+#     shift[s] = e
+
+# count[1] = 0
+# queue = deque([1])
+
+# while queue:
+#     x = queue.popleft()
+#     if x == 100:
+#         break
+    
+#     for i in range(1, 7):
+#         nx = x + i
+
+#         nx = shift[nx] if nx in shift else nx
+#         if nx < 101 and count[nx] > count[x] + 1:
+#             count[nx] = count[x] + 1
+#             queue.append(nx)
+
+# print(count[100])
+
 N, M = map(int, sys.stdin.readline().split())
 shift = {}
 count = [100] * 101
@@ -24,7 +50,10 @@ while queue:
     for i in range(1, 7):
         nx = x + i
 
-        nx = shift[nx] if nx in shift else nx
+        if nx > 100:
+            continue
+
+        nx = shift.get(nx, nx)
         if nx < 101 and count[nx] > count[x] + 1:
             count[nx] = count[x] + 1
             queue.append(nx)
