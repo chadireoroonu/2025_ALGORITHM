f4256b92cb7c130984456c2d724d6331752ab366
chadireoroonu <chadireoroonu@gmail.com>
2025-08-01 16:27:44 +0900

    2508015 python

---
+++ 08/P01_b_1927.py
@@ -0,0 +1,12 @@
+import sys, heapq
+sys.stdin = open("input.txt")
+
+N = int(sys.stdin.readline())
+queue = []
+
+for _ in range(N):
+    num = int(sys.stdin.readline())
+    if num:
+        heapq.heappush(queue, num)
+    else:
+        print(heapq.heappop(queue) if queue else 0)
