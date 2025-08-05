267a973a6d953fe02096aa0d52b51c55ae463897
chadireoroonu <chadireoroonu@gmail.com>
2025-08-05 14:49:54 +0900

    refactoring

--- 08/P05_b_1744.py
+++ 08/P05_b_1744.py
@@ -1,17 +1,13 @@
-# 수 묶기
-# https://www.acmicpc.net/problem/1744
+# # 수 묶기
+# # https://www.acmicpc.net/problem/1744
 
 import sys, heapq
 
-def sol(arr, t): # t로 양수 음수 구분
+def sol(arr, t):
     result = 0
     while len(arr) > 1:
         x = heapq.heappop(arr)
         y = heapq.heappop(arr)
-
-        if t and y > -2: # 양수면서 y가 1 이라면
-            return result - x - y - sum(arr)
-
         result += x * y
 
     return result - sum(arr) if t else result + sum(arr)
@@ -19,12 +15,42 @@ def sol(arr, t): # t로 양수 음수 구분
 N = int(sys.stdin.readline())
 plus = []
 minus = []
+one = 0
 
 for _ in range(N):
     x = int(sys.stdin.readline())
-    if x < 1:
+    if x == 1:
+        one += 1
+    elif x < 1:
         heapq.heappush(minus, x)
     else:
-        heapq.heappush(plus, -x) # 최대힙을 위한 음수처리
+        heapq.heappush(plus, -x)
 
-print(sol(plus, True) + sol(minus, False) if N > 1 else sum(minus) - sum(plus))
+print(one + sol(plus, True) + sol(minus, False) if N > 1 else sum(minus) - sum(plus))
+
+
+# def sol(arr, t): # t로 양수 음수 구분
+#     result = 0
+#     while len(arr) > 1:
+#         x = heapq.heappop(arr)
+#         y = heapq.heappop(arr)
+
+#         if t and y > -2: # 양수면서 y가 1 이라면
+#             return result - x - y - sum(arr)
+
+#         result += x * y
+
+#     return result - sum(arr) if t else result + sum(arr)
+
+# N = int(sys.stdin.readline())
+# plus = []
+# minus = []
+
+# for _ in range(N):
+#     x = int(sys.stdin.readline())
+#     if x < 1:
+#         heapq.heappush(minus, x)
+#     else:
+#         heapq.heappush(plus, -x) # 최대힙을 위한 음수처리
+
+# print(sol(plus, True) + sol(minus, False) if N > 1 else sum(minus) - sum(plus))
