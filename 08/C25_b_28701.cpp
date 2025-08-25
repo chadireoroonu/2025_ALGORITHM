bd3eea173669110b5ceaddce764880de92c5587f
chadireoroonu <chadireoroonu@gmail.com>
2025-08-25 23:28:54 +0900

    2508251 C++

---
+++ 08/C25_b_28701.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    int N;
+    cin >> N;
+
+    long long a = 0;
+    long long b = 0;
+    
+    for (int i = 1; i <= N; i++) {
+        a += i;
+        b += (long long)i * i * i;
+    }
+
+    cout << a << "\n";
+    cout << a * a << "\n";
+    cout << b << "\n";
+}
